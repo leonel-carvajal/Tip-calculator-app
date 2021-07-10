2897e90962978dc1d868c7bbea4b99b7bc834112
Leonel Carvajal <liosoft777@gmail.com>
2021-07-10 13:55:24 -0600

    Darkmode added

--- js/scritp.js
+++ js/scritp.js
@@ -9,8 +9,9 @@ const NumberOfPeople = document.querySelector('.input--people')
 const ErrorPeople = document.querySelector('.people__error')
 const Total = document.querySelector('.total__value')
 const Amount = document.querySelector('.amount__value')
+const ImgDark = document.querySelector('.header__img')
 let tip = 0
-
+let darkMode = null
 Form.addEventListener('click',(e)=>{
   e.preventDefault()
 });
@@ -18,6 +19,7 @@ const TransformToNumber = (value)=>{
   return Number.parseInt(value)
 }
 const ActiveTip = (e)=>{
+  CustomTip.value = 0
   TipValues.forEach(tip=>{
     if(tip.classList.contains('add-tip')){
       tip.classList.remove('add-tip')
@@ -26,6 +28,51 @@ const ActiveTip = (e)=>{
   e.target.classList.add('add-tip')
   tip = TransformToNumber(e.target.textContent.substring(0,2))
 }
+const CalculateTotalBill = ()=>{
+  const bill = TransformToNumber(inputBill.value)
+  const numberOfpeople = TransformToNumber(NumberOfPeople.value)
+  if(bill>0 && numberOfpeople >0 && tip >0){
+    let SubTip = bill * tip /100
+    let TipByPerson = SubTip/numberOfpeople
+    let TotalByPerson = bill /numberOfpeople
+    Amount.textContent = `${TipByPerson.toFixed(2)}$`
+    Total.textContent = `${TotalByPerson.toFixed(2)}$`
+  }
+}
+const ResetBill = ()=>{
+  TipValues.forEach(item=>item.classList.remove('add-tip'))
+  inputBill.value = 0
+  inputTip.value = 0
+  NumberOfPeople.value = 0
+  Amount.textContent = `00.0$`
+  Total.textContent = `00.0$`
+}
+
+const DarkMode = ()=>{
+    localStorage.setItem('darkMode',true)
+    darkMode = true
+    ImgDark.setAttribute('src','images/sun.svg')
+    document.documentElement.style.setProperty('--Light-grayish-cyan','rgb(13, 11, 19)')
+    document.documentElement.style.setProperty('--Dark-grayish-cyan','#fff')
+    Form.classList.add('dark--form')
+    document.querySelectorAll('.input').forEach(item=>item.classList.add('dark'))
+}
+const RemoveDarkMode = ()=>{
+  localStorage.setItem('darkMode',false)
+  darkMode=false
+  ImgDark.setAttribute('src','images/moon.svg')
+  document.documentElement.style.setProperty('--Light-grayish-cyan','hsl(189, 41%, 97%)')
+  document.documentElement.style.setProperty('--Dark-grayish-cyan','hsl(186, 14%, 43%)')
+  Form.classList.remove('dark--form')
+  document.querySelectorAll('.input').forEach(item=>item.classList.remove('dark'))
+}
+ImgDark.addEventListener('click',()=>{
+  if(darkMode){
+    RemoveDarkMode()
+  }else{
+    DarkMode()
+  }
+})
 
 TipValues.forEach(tip=>{
   tip.addEventListener('click',ActiveTip)
@@ -70,24 +117,15 @@ NumberOfPeople.addEventListener('change',(e)=>{
     e.target.classList.remove('error')
   }
 })
-Calculate.addEventListener('click',()=>{
-    const bill = TransformToNumber(inputBill.value)
-    const numberOfpeople = TransformToNumber(NumberOfPeople.value)
-    if(bill>0 && numberOfpeople >0 && tip >0){
-      let SubTip = bill * tip /100
-      let TipByPerson = SubTip/numberOfpeople
-      let TotalByPerson = bill /numberOfpeople
-      Amount.textContent = `${TipByPerson.toFixed(2)}$`
-      Total.textContent = `${TotalByPerson.toFixed(2)}$`
-    }
-})
-Reset.addEventListener('click',()=>{
-  TipValues.forEach(item=>item.classList.remove('add-tip'))
-  inputBill.value = 0
-  inputTip.value = 0
-  NumberOfPeople.value = 0
-  Amount.textContent = `00.0$`
-  Total.textContent = `00.0$`
-})
+
+Calculate.addEventListener('click',CalculateTotalBill)
+Reset.addEventListener('click',ResetBill)
 
 
+window.addEventListener('DOMContentLoaded',()=>{
+  if(localStorage.getItem('darkMode')===true){
+    RemoveDarkMode()
+  }else{
+    DarkMode()
+  }
+})
